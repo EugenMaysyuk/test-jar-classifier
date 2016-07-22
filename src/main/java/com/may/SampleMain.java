7698fee98b7398fe502da9fe401def46db62e8b4
Eugen Maysyuk <eugenmaysyuk@coherentsolutions.com>
2016-07-22 16:53:30 +0300

    The classifier cannot be configured for test-jar goal

---
+++ src/main/java/com/may/SampleMain.java
@@ -0,0 +1,9 @@
+package com.may;
+
+/**
+ * Simple main class.
+ *
+ * Created by EugenMaysyuk on 7/22/2016.
+ */
+public class SampleMain {
+}
---
+++ src/main/test/com/may/SampleTest.java
@@ -0,0 +1,9 @@
+package com.may;
+
+/**
+ * Simple test.
+ *
+ * Created by EugenMaysyuk on 7/22/2016.
+ */
+public class SampleTest {
+}
